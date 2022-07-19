1e8cb9771a5d8b976423756dfeed0b9f57a08b7b
Adam Gonzales <adamgonzales1@gmail.com>
2022-07-18 20:07:53 -0700

    feature: add ability to log-on and update registration data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,33 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { signOut, onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebase'
+import { useNavigate } from 'react-router-dom'
 
 export default function Homepage() {
-  return <div>Homepage</div>
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (!user) {
+        navigate('/')
+      }
+    })
+  }, [])
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate('/')
+      })
+      .catch((err) => {
+        alert(err.message)
+      })
+  }
+
+  return (
+    <div>
+      <h1>Test</h1>
+      <button onClick={handleSignOut}>Sign out</button>
+    </div>
+  )
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,46 +1,131 @@
 import { useState, useEffect } from 'react'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth'
+import { auth } from '../firebase'
+import { useNavigate } from 'react-router-dom'
 
 export default function Welcome() {
   const [userData, setUserData] = useState({
     email: '',
     password: '',
   })
+  const [registrationData, setRegistrationData] = useState({
+    email: '',
+    confirmEmail: '',
+    password: '',
+    confirmPassword: '',
+  })
 
-  console.log(userData)
+  const [isRegistering, setIsRegistering] = useState(false)
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigate('/homepage')
+      }
+    })
+  }, [])
+
+  console.log(registrationData)
 
   function handleInputChange(e) {
     const { name, value } = e.target
-    setUserData((prevData) => {
-      return {
-        ...prevData,
-        [name]: value,
-      }
+    if (!isRegistering) {
+      setUserData((prevData) => {
+        return {
+          ...prevData,
+          [name]: value,
+        }
+      })
+    } else {
+      setRegistrationData((prevData) => {
+        return {
+          ...prevData,
+          [name]: value,
+        }
+      })
+    }
+  }
+
+  function handleSignIn() {
+    signInWithEmailAndPassword(auth, userData.email, userData.password)
+      .then(() => navigate('/homepage'))
+      .catch((err) => alert(err.message))
+  }
+
+  function handleRegister() {
+    createUserWithEmailAndPassword(
+      auth,
+      userData.email,
+      userData.password
+    ).then(() => {
+      navigate('/homepage')
     })
   }
 
   return (
     <div className='welcome'>
-      <div className='loginContainer'>
-        <input
-          name='email'
-          type='email'
-          value={userData.email}
-          placeholder='email'
-          onChange={handleInputChange}
-        />
-        <input
-          name='password'
-          type='password'
-          value={userData.password}
-          placeholder='password'
-          onChange={handleInputChange}
-        />
-        <button>Sign in</button>
-        <a href=''>Create an account</a>
+      <div className='login-register-container'>
+        {isRegistering ? (
+          <>
+            <input
+              name='email'
+              type='email'
+              value={registrationData.email}
+              placeholder='Email'
+              onChange={handleInputChange}
+            />
+            <input
+              name='confirmEmail'
+              type='email'
+              value={registrationData.confirmEmail}
+              placeholder='Confirm Email'
+              onChange={handleInputChange}
+            />
+            <input
+              name='password'
+              type='password'
+              value={registrationData.password}
+              placeholder='Password'
+              onChange={handleInputChange}
+            />
+            <input
+              name='confirmPassword'
+              type='password'
+              value={registrationData.confirmPassword}
+              placeholder='Confirm Password'
+              onChange={handleInputChange}
+            />
+            <button onClick={handleRegister}>Register</button>
+            <button onClick={() => setIsRegistering(false)}>Go Back</button>
+          </>
+        ) : (
+          <>
+            <input
+              name='email'
+              type='email'
+              value={userData.email}
+              placeholder='email'
+              onChange={handleInputChange}
+            />
+            <input
+              name='password'
+              type='password'
+              value={userData.password}
+              placeholder='password'
+              onChange={handleInputChange}
+            />
+            <button onClick={handleSignIn}>Sign in</button>
+            <button onClick={() => setIsRegistering(true)}>
+              Create an account
+            </button>
+          </>
+        )}
       </div>
     </div>
   )
